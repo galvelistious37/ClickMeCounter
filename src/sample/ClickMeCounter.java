25510c6ad38b2d2fd1bff9170b1d1726a2764d69
galvelistious37 <galvelistious37@gmail.com>
2021-01-07 21:48:18 -0800

    Added the quit button and functionality to quit program

--- src/sample/ClickMeCounter.java
+++ src/sample/ClickMeCounter.java
@@ -13,22 +13,24 @@ public class ClickMeCounter extends Application {
         launch(args);
     }
 
-    Button btn;
-    Label lbl;
-    int clickCounter = Constants.ZERO;
+    private Label lbl;
+    private int clickCounter = Constants.ZERO;
 
     @Override
     public void start(Stage primaryStage){
-        btn = new Button(Constants.CLICK_ME);
+        Button btn = new Button(Constants.CLICK_ME);
+        Button quitButton = new Button(Constants.QUIT);
         lbl = new Label(Constants.NO_CLICKS);
 
         // Create button and label
         btn.setOnAction(e -> buttonClick());
+        quitButton.setOnAction(e -> quit());
 
         // Set button and label to pane
         BorderPane pane = new BorderPane();
         pane.setTop(lbl);
         pane.setCenter(btn);
+        pane.setBottom(quitButton);
 
         // Add pane to a scene
         Scene scene = new Scene(pane, 350, 250);
@@ -43,5 +45,9 @@ public class ClickMeCounter extends Application {
         clickCounter++;
         String message = clickCounter > 1 ? Constants.MORE_THAN_ONE_FIRST + clickCounter + Constants.MORE_THAN_ONE_LAST : Constants.ONE_CLICK;
         lbl.setText(message);
+    }
+
+    private void quit(){
+        System.exit(0);
     }
 }
